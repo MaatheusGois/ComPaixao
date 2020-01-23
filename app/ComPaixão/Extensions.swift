50f26decaaa4af48a68a059a8c2af72062b679a8
Gois <matheusgoislimasilva@gmail.com>
2020-01-22 22:03:57 -0300

    trocar nome da pasta central para atual nome do app

---
+++ app/ComPaixão/Extensions.swift
@@ -0,0 +1,74 @@
+//
+//  Extensions.swift
+//  PropositoClient
+//
+//  Created by Matheus Gois on 02/07/19.
+//  Copyright © 2019 Matheus Gois. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Subtract Dates
+extension Date {
+    static func - (lhs: Date, rhs: Date) -> TimeInterval {
+        return lhs.timeIntervalSinceReferenceDate - rhs.timeIntervalSinceReferenceDate
+    }
+}
+
+//Format Date
+extension Date {
+    static func getFormattedDate(date: Date) -> String{
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "dd MMM yyyy - HH:mm"
+        return dateFormatterPrint.string(from: date) // Feb 01,2018
+    }
+}
+
+//Random int
+extension Int {
+    static func gererateId() -> Int {
+        return Int.random(in: 0..<100000000)
+    }
+}
+
+
+//Round Button
+@IBDesignable class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+    }
+    
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+    
+}
+
+//Alert
+extension UIView {
+    private func createAlert(_ title:String,_ msg:String){
+        
+    }
+}
